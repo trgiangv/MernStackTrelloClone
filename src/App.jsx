e391b1ba1d11e2f22c0fb530a23fc8848fdbf955
Vu Truong Giang <85680448+trgiangv@users.noreply.github.com>
2024-06-27 00:07:09 +0700

    fix: css base line + import error

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import Button from '@mui/material'
+import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import HomeIcon from '@mui/icons-material/Home'
-import pink from '@mui/material/colors'
+import { pink } from '@mui/material/colors'
 
 function App() {
-
-
   return (
     <>
       <div>giang vu</div>
-      <Button variant="text">Text</Button>
+      <Button variant="text">xin chao</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
+      <Button variant="outlined">313123</Button>
 
       <br />
       <AccessAlarmIcon />
